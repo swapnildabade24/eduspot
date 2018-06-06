d71e6466e07c1e57a4d7c07adb7e91b29c277877
swapnildababade24 <swapnildabade24@gmail.com>
2018-06-06 09:36:15 +0530

    Template added

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	@include('layouts.head')
+</head>
+
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+	@include('layouts.header')
+
+	@include('layouts.footer')
+
+
+</body>
+</html>	
